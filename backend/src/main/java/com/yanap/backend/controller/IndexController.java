3aaa4e8e3f010da4c8286c81356cc0ff3847a071
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-24 23:45:08 +0900

    アプリケーション全体でCORS対応するようにした

--- backend/src/main/java/com/yanap/backend/controller/IndexController.java
+++ backend/src/main/java/com/yanap/backend/controller/IndexController.java
@@ -2,11 +2,9 @@ package com.yanap.backend.controller;
 
 import com.yanap.backend.response.MessageResponse;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
 @RestController
 public class IndexController {
     @RequestMapping("/")
