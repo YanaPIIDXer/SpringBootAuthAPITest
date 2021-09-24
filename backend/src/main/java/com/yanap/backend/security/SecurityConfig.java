e821ed88f24361e8cfa4e40998bd042784f57e9a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-24 21:47:12 +0900

    ユーザサービスを設定

--- backend/src/main/java/com/yanap/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/yanap/backend/security/SecurityConfig.java
@@ -1,6 +1,10 @@
 package com.yanap.backend.security;
 
+import com.yanap.backend.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,8 +12,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserService userService;
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         
+    }
+    
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService);
     }
 }
