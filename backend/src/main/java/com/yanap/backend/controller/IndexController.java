e4e8ea456b591b1af4669782affe81016fddddff
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-24 22:11:26 +0900

    CORS対応

--- backend/src/main/java/com/yanap/backend/controller/IndexController.java
+++ backend/src/main/java/com/yanap/backend/controller/IndexController.java
@@ -2,9 +2,11 @@ package com.yanap.backend.controller;
 
 import com.yanap.backend.response.IndexResponse;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class IndexController {
     @RequestMapping("/")
