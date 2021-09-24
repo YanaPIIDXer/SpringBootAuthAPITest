44a62d42b36743bc4aeaaa0efcdb9c85ff106517
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-24 23:53:52 +0900

    ログイン・ログアウトの設定

--- backend/src/main/java/com/yanap/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/yanap/backend/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -19,7 +20,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.mvcMatcher("/auth/**")
                 .authorizeRequests()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+        .and()
+            .formLogin()
+                .loginProcessingUrl("/auth/login")
+                .usernameParameter("name")
+                .passwordParameter("password")
+                .permitAll()
+        .and()
+            .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout"));
     }
     
     @Override
